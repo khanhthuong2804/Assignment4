bb7d8ddbf379a91accb132ce85eefa3a68995f4e
khanhthuong2804 <61644948+khanhthuong2804@users.noreply.github.com>
2020-05-11 15:47:55 +0200

    Try it out

---
+++ R/hello.R
@@ -0,0 +1,87 @@
+
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+
+
+# title function: fars_read of package Assignment4
+#' @description
+# check existence of data file. if the file doesn't exist there is stop message
+#' @param filename a string with the file name
+#' @return to return the file
+#' @examples
+#' \dontrun
+#' {
+# fars_read("accident_2015.csv.bz2")
+#' }
+#' @importFrom readr read_csv
+#' @importFrom dplyr tbl_df
+#' @export    export the function, so users will have direct access to it when they load the package
+
+system.file("extdata","Assignment4", package = "Assignment4")
+
+
+
+fars_read <- function(filename) {
+  if(!file.exists(filename))
+    stop("file '", filename, "' does not exist")
+  data <- suppressMessages({
+    readr::read_csv(filename, progress = FALSE)
+  })
+  dplyr::tbl_df(data)
+}
+make_filename <- function(year) {
+  year <- as.integer(year)
+  sprintf("accident_%d.csv.bz2", year)
+}
+fars_read_years <- function(years) {
+  lapply(years, function(year) {
+    file <- make_filename(year)
+    tryCatch({
+      dat <- fars_read(file)
+      dplyr::mutate(dat, year = year) %>%
+        dplyr::select(MONTH, year)
+    }, error = function(e) {
+      warning("invalid year: ", year)
+      return(NULL)
+    })
+  })
+}
+fars_summarize_years <- function(years) {
+  dat_list <- fars_read_years(years)
+  dplyr::bind_rows(dat_list) %>%
+    dplyr::group_by(year, MONTH) %>%
+    dplyr::summarize(n = n()) %>%
+    tidyr::spread(year, n)
+}
+fars_map_state <- function(state.num, year) {
+  filename <- make_filename(year)
+  data <- fars_read(filename)
+  state.num <- as.integer(state.num)
+
+  if(!(state.num %in% unique(data$STATE)))
+    stop("invalid STATE number: ", state.num)
+  data.sub <- dplyr::filter(data, STATE == state.num)
+  if(nrow(data.sub) == 0L) {
+    message("no accidents to plot")
+    return(invisible(NULL))
+  }
+  is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
+  is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
+  with(data.sub, {
+    maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
+              xlim = range(LONGITUD, na.rm = TRUE))
+    graphics::points(LONGITUD, LATITUDE, pch = 46)
+  })
+}
+
+
+use_vignette("model_details")
+
+
+devtools::install_github()
+
+
---
+++ R/testthat.R
@@ -0,0 +1,10 @@
+library(testthat)
+library(Assignment4)
+
+
+test_that("fars_read_years", {
+  # warning when a year does not exist
+  yrs <- 2012:2015
+  expect_warning(fars_read_years(yrs),
+                 "invalid year: 2012")
+})
