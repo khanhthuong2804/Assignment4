1e308dbaa13b0684c4f42cfc6ac6e8c2fef05b80
khanhthuong2804 <61644948+khanhthuong2804@users.noreply.github.com>
2020-05-12 08:28:16 +0200

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -78,14 +78,4 @@ fars_map_state <- function(state.num, year) {
   })
 }
 
-#create vignette
-use_vignette("model_details")
-
-#create travis
-
-library(devtools)
-use_travis()
-
-#create readme
-use_readme_md(open = rlang::is_interactive())
 
